61265fc9ebabcfd13bfd2c77e0a24054ef066fe7
thiagolopess <thiago.lopes.santos.tls@gmail.com>
2023-04-04 22:22:06 -0300

    Adds api secret key to configuration class

--- src/config.py
+++ src/config.py
@@ -6,6 +6,7 @@ basedir = os.path.abspath(os.path.dirname(__file__))
 class Config(object):
     TESTING = True
     CORS_ORIGINS = "*"
+    SECRET_KEY = os.getenv("SECRET_KEY")
     SQLALCHEMY_DATABASE_URI = os.getenv("DATABASE_URL")
     SQLALCHEMY_TRACK_MODIFICATIONS = False
 
