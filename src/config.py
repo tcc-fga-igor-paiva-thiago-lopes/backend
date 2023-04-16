d919f15bc54ab017541f56fd900838e61bf273f0
thiagolopess <thiago.lopes.santos.tls@gmail.com>
2023-04-16 14:22:06 -0300

    Adds configuration so the session token doesn't expire once created

--- src/config.py
+++ src/config.py
@@ -6,6 +6,7 @@ basedir = os.path.abspath(os.path.dirname(__file__))
 class Config(object):
     CORS_ORIGINS = "*"
     JWT_SECRET_KEY = os.getenv("SECRET_KEY")
+    JWT_ACCESS_TOKEN_EXPIRES = False
     SQLALCHEMY_DATABASE_URI = os.getenv("DATABASE_URL")
     SQLALCHEMY_TRACK_MODIFICATIONS = False
 
@@ -13,5 +14,6 @@ class Config(object):
 class TestConfig(object):
     TESTING = True
     JWT_SECRET_KEY = "chave_secreta"
+    JWT_ACCESS_TOKEN_EXPIRES = False
     SQLALCHEMY_DATABASE_URI = "sqlite://"
     SQLALCHEMY_TRACK_MODIFICATIONS = False
