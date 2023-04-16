c0ae8baf9f0ebde2d3de07e9c948e2584b98ff58
thiagolopess <thiago.lopes.santos.tls@gmail.com>
2023-04-16 13:53:33 -0300

    Sets up flask-jwt-extended library

--- src/config.py
+++ src/config.py
@@ -5,7 +5,7 @@ basedir = os.path.abspath(os.path.dirname(__file__))
 
 class Config(object):
     CORS_ORIGINS = "*"
-    SECRET_KEY = os.getenv("SECRET_KEY")
+    JWT_SECRET_KEY = os.getenv("SECRET_KEY")
     SQLALCHEMY_DATABASE_URI = os.getenv("DATABASE_URL")
     SQLALCHEMY_TRACK_MODIFICATIONS = False
 
