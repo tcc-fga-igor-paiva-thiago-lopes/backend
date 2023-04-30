07eeb4f11f1d2a44701440430f1c33a2fad263ad
thiagolopess <thiago.lopes.santos.tls@gmail.com>
2023-04-30 15:37:53 -0300

    Adds Category model

---
+++ src/models/category.py
@@ -0,0 +1,26 @@
+from src.app import db
+from sqlalchemy import LargeBinary
+from .application_model import ApplicationModel
+
+
+class TruckDriver(ApplicationModel):
+    __tablename__ = "TRUCK_DRIVER"
+
+    FRIENDLY_NAME_SINGULAR = "Categoria"
+    FRIENDLY_NAME_PLURAL = "Categorias"
+
+    name = db.Column(db.String(60), nullable=False, unique=True)
+    color = db.Column(LargeBinary, nullable=False)
+
+    def __init__(self, **kwargs):
+        color = kwargs.pop("color", None)
+
+        if len(color or "") == 0:
+            raise Exception("Color is required")
+
+        color = bytes.fromhex(color)
+
+        return super().__init__(**kwargs, color=color)
+
+    def get_color(self):
+        return self.color.hex()
