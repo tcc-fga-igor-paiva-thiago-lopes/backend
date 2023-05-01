7f8a690f392623816d33831f35fe5ae2bdca0e7e
thiagolopess <thiago.lopes.santos.tls@gmail.com>
2023-05-01 19:41:10 -0300

    Removes categories unnecessary imports

--- src/controllers/categories.py
+++ src/controllers/categories.py
@@ -1,20 +1,7 @@
-import requests
 from flask_restful import Api
-from flask_jwt_extended import (
-    create_access_token,
-    jwt_required,
-    current_user,
-)
-from sqlalchemy.exc import IntegrityError
 from flask import Blueprint
-from marshmallow import ValidationError
-from src.app import db, jwt
 from src.models.category import Category
 from src.controllers.common.group_api import GroupAPI
-from src.controllers.common.utils import (
-    simple_error_response,
-    validation_error_response,
-)
 from src.schemas.category_schema import CategorySchema
 
 PERMITTED_PARAMS = ["name", "color"]
