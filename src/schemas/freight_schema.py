d35bfb8c721de2023bae564eaada74d983572578
igor-paiva <igorbatistapaiva@outlook.com>
2023-06-01 08:43:04 -0300

    Accept freight cargo enum by value

--- src/schemas/freight_schema.py
+++ src/schemas/freight_schema.py
@@ -1,10 +1,14 @@
-from src.models.freight import Freight
+from marshmallow import fields
+
 from src.schemas.base_schema import BaseSchema
+from src.models.freight import Freight, FreightCargoEnum
 
 
 class FreightSchema(BaseSchema):
     class Meta(BaseSchema.Meta):
         model = Freight
+
+    cargo = fields.Enum(FreightCargoEnum, by_value=True)
 
 
 freight_schema = FreightSchema()
