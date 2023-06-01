2168b849c75a658f6e967c4bcefb5cd050c40300
igor-paiva <igorbatistapaiva@outlook.com>
2023-05-31 23:21:11 -0300

    Change freight cargo to be an enum

--- src/models/freight.py
+++ src/models/freight.py
@@ -1,8 +1,27 @@
+import enum
+
 from src.app import db
 
 # Must import, so the relationship is loaded properly
 from .truck_driver import TruckDriver
 from .application_model import ApplicationModel
+
+
+class FreightCargoEnum(str, enum.Enum):
+    GENERAL = "Geral"
+    CONTAINERIZED = "Conteinerizada"
+    REFRIGERATED = "Frigorificada"
+    LIQUID_BULK = "Granel Líquido"
+    PRESSURIZED_BULK = "Granel Pressurizada"
+    SOLID_BULK = "Granel Sólido"
+    NEW_BULK = "Neogranel"
+    DANGER = "Frigorificada"
+    DANGEROUS_GENERAL = "Perigosa Geral"
+    DANGEROUS_CONTAINERIZED = "Perigosa Conteinerizada"
+    DANGEROUS_REFRIGERATED = "Perigosa Frigorificada"
+    DANGEROUS_LIQUID_BULK = "Perigosa Granel Líquido"
+    DANGEROUS_PRESSURIZED_BULK = "Perigosa Granel Pressurizada"
+    DANGEROUS_SOLID_BULK = "Perigosa Granel Sólido"
 
 
 class Freight(ApplicationModel):
@@ -11,25 +30,25 @@ class Freight(ApplicationModel):
     FRIENDLY_NAME_SINGULAR = "Frete"
     FRIENDLY_NAME_PLURAL = "Fretes"
 
-    cargo = db.Column(db.String(50), nullable=False)
+    cargo = db.Column(db.Enum(FreightCargoEnum), nullable=False)
     description = db.Column(db.String(500), nullable=False)
     contractor = db.Column(db.String(60), nullable=False)
     cargo_weight = db.Column(db.Numeric(8, 2), nullable=False)
     agreed_payment = db.Column(db.Numeric(8, 2), nullable=False)
     distance = db.Column(db.Numeric(8, 2), nullable=False)
 
-    start_date = db.Column(db.DateTime(timezone=True), nullable=False)
+    start_date = db.Column(db.DateTime(timezone=True))
     due_date = db.Column(db.DateTime(timezone=True))
     finished_date = db.Column(db.DateTime(timezone=True))
 
-    origin_city = db.Column(db.String(50))
-    origin_state = db.Column(db.CHAR(2))
+    origin_city = db.Column(db.String(50), nullable=False)
+    origin_state = db.Column(db.CHAR(2), nullable=False)
     origin_country = db.Column(db.String(50))
     origin_latitude = db.Column(db.Numeric(9, 6))
     origin_longitude = db.Column(db.Numeric(9, 6))
 
-    destination_city = db.Column(db.String(50))
-    destination_state = db.Column(db.CHAR(2))
+    destination_city = db.Column(db.String(50), nullable=False)
+    destination_state = db.Column(db.CHAR(2), nullable=False)
     destination_country = db.Column(db.String(50))
     destination_latitude = db.Column(db.Numeric(9, 6))
     destination_longitude = db.Column(db.Numeric(9, 6))
