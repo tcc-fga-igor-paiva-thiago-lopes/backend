e69349a6608b8155bc1ef521e0ed3b60f9a4075c
igor-paiva <igorbatistapaiva@outlook.com>
2023-06-11 19:37:15 -0300

    Creates endpoint to sync records in group API

--- src/controllers/common/group_api.py
+++ src/controllers/common/group_api.py
@@ -1,4 +1,6 @@
+import pytz
 import requests
+from datetime import datetime
 from flask_restful import Resource
 from flask import request, make_response
 from flask_jwt_extended import current_user
@@ -6,7 +8,7 @@ from flask_jwt_extended import jwt_required
 
 
 from src.app import db
-from src.controllers.common.utils import permitted_parameters
+from src.controllers.common.utils import permitted_parameters, simple_error_response
 
 
 class GroupAPI(Resource):
@@ -34,6 +36,24 @@ class GroupAPI(Resource):
 
         return db.session.execute(db.select(self.model)).scalars()
 
+    def _add_fields_to_import_data(self, data):
+        params = permitted_parameters(data, self.permitted_params)
+
+        if self.user_association_fk is not None:
+            params[self.user_association_fk] = current_user.id
+
+        params["synced_at"] = datetime.utcnow().replace(tzinfo=pytz.utc).isoformat()
+
+        return params
+
+    def _process_import_fields(self, import_data):
+        return list(
+            map(
+                lambda freight_data: self._add_fields_to_import_data(freight_data),
+                import_data,
+            )
+        )
+
     def get(self):
         items = self._get_records()
 
@@ -51,3 +71,21 @@ class GroupAPI(Resource):
         item.save()
 
         return make_response(self.item_schema.dump(item), requests.codes.created)
+
+    def patch(self):
+        import_data = self._process_import_fields(request.get_json(force=True))
+
+        # Using group_schema to validate fields
+        self.group_schema.load(import_data)
+
+        if len(import_data) == 0:
+            return simple_error_response(
+                "Nenhum registro a importar", requests.codes.bad_request
+            )
+
+        ret = self.model.upsert(data=import_data, unique_by=["identifier"])
+
+        if len(import_data) == len(ret):
+            return make_response(ret, requests.codes.ok)
+
+        return make_response(ret, requests.codes.accepted)
