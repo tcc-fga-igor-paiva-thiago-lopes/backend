4025b6ed925e6340170f4d263b1264993601bd6c
thiagolopess <thiago.lopes.santos.tls@gmail.com>
2023-07-10 00:26:05 -0300

    Update group api

--- src/controllers/common/group_api.py
+++ src/controllers/common/group_api.py
@@ -1,71 +1,130 @@
+import pytz
 import requests
+from datetime import datetime
 from flask_restful import Resource
 from flask import request, make_response
-from flask_jwt_extended import jwt_required, current_user
+from flask_jwt_extended import current_user
+from flask_jwt_extended import jwt_required
 
-from src.app import db, jwt
-from src.controllers.common.utils import permitted_parameters
-from src.models.truck_driver import TruckDriver
+
+from src.app import db
+from src.controllers.common.utils import permitted_parameters, simple_error_response
 
 
 class GroupAPI(Resource):
     init_every_request = False
+    decorators = [jwt_required()]
 
-    def __init__(self, model, permitted_params, model_schema):
+    def __init__(
+        self,
+        model,
+        permitted_params,
+        model_schema,
+        list_query=None,
+        user_association_fk=None,
+    ):
         self.model = model
         self.permitted_params = permitted_params
         self.item_schema = model_schema()
         self.group_schema = model_schema(many=True)
+        self.list_query = list_query
+        self.user_association_fk = user_association_fk
 
-    @jwt_required()
-    def get(self):
-        id = request.args.get("id")
-        if id is not None and id != "":
-            item = db.get_or_404(
-                self.model,
-                request.args.get("id"),
-                description=f"{self.model.FRIENDLY_NAME_SINGULAR} não encontrado(a).",
+    def _get_records(self):
+        if self.list_query is not None:
+            return self.list_query(current_user)
+
+        return db.session.execute(db.select(self.model)).scalars()
+
+    def _add_fields_to_import_data(self, data):
+        params = permitted_parameters(data, self.permitted_params)
+
+        if self.user_association_fk is not None:
+            params[self.user_association_fk] = current_user.id
+
+        params["synced_at"] = datetime.utcnow().replace(tzinfo=pytz.utc).isoformat()
+
+        return params
+
+    def _process_import_fields(self, import_data):
+        return list(
+            map(
+                lambda freight_data: self._add_fields_to_import_data(freight_data),
+                import_data,
             )
+        )
 
-            return make_response(self.item_schema.dump(item), requests.codes.ok)
-
-        items = db.session.execute(db.select(self.model)).scalars()
+    def get(self):
+        items = self._get_records()
 
         return make_response(self.group_schema.dump(items), requests.codes.ok)
 
-    @jwt_required()
     def post(self):
         request_data = request.get_json(force=True)
 
-        item = self.item_schema.load(
-            permitted_parameters(request_data, self.permitted_params)
-        )
+        params = permitted_parameters(request_data, self.permitted_params)
+
+        if self.user_association_fk is not None:
+            params[self.user_association_fk] = current_user.id
+
+        item = self.item_schema.load(params)
         item.save()
 
         return make_response(self.item_schema.dump(item), requests.codes.created)
 
-    @jwt_required()
-    def delete(self):
-        id = request.args.get("id")
-        if id is None or id == "":
-            return make_response({"message": "ID não informado."}, requests.codes.bad)
+    def patch(self):
+        import_data = self._process_import_fields(request.get_json(force=True))
 
-        item = db.get_or_404(
-            self.model,
-            request.args.get("id"),
-            description=f"{self.model.FRIENDLY_NAME_SINGULAR} não encontrado(a)",
+        # Using group_schema to validate fields
+        self.group_schema.load(import_data, partial=True)
+
+        if len(import_data) == 0:
+            return simple_error_response(
+                "Nenhum registro a sincronizar", requests.codes.bad_request
+            )
+
+        upserted_identifiers = self.model.upsert(
+            data=import_data, unique_by=["identifier"]
         )
 
-        item.destroy()
+        if len(upserted_identifiers) == 0:
+            return simple_error_response(
+                "Não foi possível remover nenhum registro", requests.codes.bad_request
+            )
+
+        if len(import_data) == len(upserted_identifiers):
+            return make_response(upserted_identifiers, requests.codes.ok)
+
+        return make_response(upserted_identifiers, requests.codes.ok)
+
+    def delete(self):
+        identifiers = request.args.getlist("identifiers")
+
+        if len(identifiers) == 0:
+            return simple_error_response(
+                "Nenhum registro a remover", requests.codes.bad_request
+            )
+
+        deleted_identifiers = self.model.destroy_by_identifiers(identifiers)
+
+        if len(identifiers) == len(deleted_identifiers):
+            return make_response(
+                {"deleted": deleted_identifiers, "not_exists": []}, requests.codes.ok
+            )
+
+        error_identifiers = set(identifiers) - set(deleted_identifiers)
+
+        existing_identifiers = set(
+            db.session.execute(
+                db.select(self.model.identifier).where(
+                    self.model.identifier.in_(error_identifiers)
+                )
+            ).scalars()
+        )
+
+        not_existing_identifiers = list(error_identifiers - existing_identifiers)
 
         return make_response(
-            {
-                "message": f"{self.model.FRIENDLY_NAME_SINGULAR} deletado(a) com sucesso."
-            },
+            {"deleted": deleted_identifiers, "not_exists": not_existing_identifiers},
             requests.codes.ok,
         )
-
-    @jwt.user_lookup_loader
-    def user_lookup_callback(_jwt_header, jwt_data):
-        identity = jwt_data["sub"]
-        return db.get_or_404(TruckDriver, identity)
